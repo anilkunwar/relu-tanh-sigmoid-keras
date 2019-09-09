087fa31b083714bc8bad122c9b5990ca13695435
Christian Versloot <mail@christianversloot.nl>
2019-09-09 09:05:34 +0200

    Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# relu-tanh-sigmoid-keras
+Keras neural network implementations for ReLU, Tanh and Sigmoid activation functions.
+
+Read more on Machine Curve: https://www.machinecurve.com/index.php/2019/09/09/implementing-relu-sigmoid-and-tanh-in-keras/
+
+Dataset: https://www.kaggle.com/uciml/pima-indians-diabetes-database
---
+++ sigmoid.py
@@ -0,0 +1,25 @@
+# Load dependencies
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+
+# Load data
+dataset = np.loadtxt('./pima_dataset.csv', delimiter=',')
+
+# Separate input data and target data
+X = dataset[:, 0:8]
+Y = dataset[:, 8]
+
+# Create the Perceptron
+model = Sequential()
+
+# Model layers
+model.add(Dense(12, input_shape=(8,), activation='sigmoid'))
+model.add(Dense(8, activation='sigmoid'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Model compilation
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Model training
+model.fit(X, Y, epochs=225, batch_size=25, verbose=1, validation_split=0.2)
